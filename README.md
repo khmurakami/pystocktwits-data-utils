d2d5468f9a857297eadf2f92663d174b80b8945e
Kalani Murakami <kalanimurakami1218@gmail.com>
2019-02-05 00:30:36 -0800

    Started test script, added to gitignore

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # pystocktwits_data_utils
+
+Data Tools for the Json outputs from the pystocktwits wrapper
+
+
+## Update Submodule
+```shell
+git submodule update --remote --merge
+```
---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="pystocktwits_data_utils",
+    version="0.0.3",
+    author="Kalani Murakami",
+    author_email="kalanimurakami1218@gmail.com",
+    description="Data Utils for Unofficial Python Wrapper StockTwits API",
+    packages=['pystocktwits', 'pystocktwits_data_utils'],
+    install_requires=["requests"],
+    license="MIT",
+    url="https://github.com/khmurakami/pystocktwits"
+)
